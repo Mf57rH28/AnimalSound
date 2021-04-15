9d5a7e7652be3ae394b37e673f00aec1981d6726
ASUSM <ASUSM@192.168.1.16>
2021-04-15 15:19:57 +0300

    Animal Sound

---
+++ AnimalSound/src/Main.java
@@ -0,0 +1,13 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Pig myPig = new Pig(); // Create a Pig object
+		myPig.animalSound();
+		myPig.sleep();
+
+	}
+
+}
